e84b1cc2334c95db2cea1db58bd1de9e1a97cf96
Cory W. Cordell <cwcordell@yahoo.com>
2016-12-05 12:46:59 -0600

    update

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,7 @@
 /**
  * Created by cwcordell on 9/27/16.
 */
-
+const data;
 /**
  * Click event listener for the latitude and longitude user input submission.
  * The function validates the input then either calls the notify function if
@@ -31,53 +31,19 @@ document.getElementById('lat_long_submit').addEventListener('click', function (e
         if(!latRegex.test(lat) || !longRegex.test(long)) {
             er.push('The coordinates are in an improper format');
         }
-        console.log(latRegex.test(lat));
-        console.log(longRegex.test(long));
 
-        // need to validate data here
         // lat and long limits:
         // lat: 90S (-90) to 90N (+90)
         // long: 180W (-180) to 180E (+180)
-    //     console.log(lat + ' ' + long);
-    //     // check lat and long direction
+
+        // check lat and long direction
         var latDir = lat.slice(-1);
         var longDir = long.slice(-1);
-    //     if(latDir !== 'N' && latDir !== 'S') er.push('Latitude must end with an N or S');
-    //     if(longDir !== 'E' && latDir !== 'W') er.push('Longitude must end with an E or W');
-    //
-    //     // check if lat and long input is a valid float value
         var latNumString = lat.slice(0,-1).trim();
         var latNums = parseFloat(latNumString);
         var longNumString = long.slice(0,-1).trim();
         var longNums = parseFloat(longNumString);
-        // var latArray = latNumString.split('.');
-        // var longArray = longNumString.split('.');
-    //
-    //     if(latNums.toString().length !== latNumString.length) {
-    //         er.push('Latitude coordinate does not contain a valid number');
-    //     }
-    //
-    //     if(longNums.toString().length !== longNumString.length) {
-    //         er.push('Longitude coordinate does not contain a valid number');
-    //     }
-    //
-    //     // check incrementation
-    //     // if(latArray[1].length > 1 || (latArray[1] !== 0 || latArray[1] !== 5))
-    //     //     er.push('Latitude must be in increments of .5 degrees');
-    //     // if(longArray[1].length > 1 || (longArray[1] !== 0 || longArray[1] !== 5))
-    //     //     er.push('Longitude must be in increments of .5 degrees');
-    //
-    //     // check ranges
-    //     if(!(latNums <= 90 && latNums >= -90)) er.push('Latitude is out of range, must be between -90 and 90');
-    //     if(!(longNums <= 180 && longNums >= -180)) er.push('Longitude is out of range, must be between -180 and 180');
-    //
-    //     // console.log(latNums.toString().length);
-    //     // console.log(latNumString.length);
-    //     // console.log(typeof latNums);
-    //     // console.log(latNums + ' ' + longNums);
-    //     // console.log(latDir + ' ' + longDir);
-    //     // console.log(lat + ' ' + long);
-    //     // console.log(er);
+
         var minLong = longNums;
         var maxLat = latNums;
         var qArray = [];
@@ -112,22 +78,30 @@ document.getElementById('lat_long_submit').addEventListener('click', function (e
     }
 });
 
-function data(i1, i2, i3, lat, long, date) {
-    this.lati = i1;
-    this.longi = i2;
-    this.i = i3;
-    this.lat = lat;
-    this.long = long;
-    this.date = date;
-}
-
-function parse(ar) {
-    var d = {};
-    d['1.2.3'] = new data(1,2,3,4,43,84);
-    for(var x in ar) {
-        d['1.2.3'] = new data(1,2,3,4,43,84);
-    }
-}
+// function data_obj(i1, i2, i3, lat, long, date) {
+//     this.lati = i1;
+//     this.longi = i2;
+//     this.i = i3;
+//     this.lat = lat;
+//     this.long = long;
+//     this.date = date;
+// }
+//
+// function lat5_obj(y) {
+//     this.lat5 = y;
+// }
+//
+// function long5_obj(x) {
+//     this.long5 = x;
+// }
+//
+// function parse(ar) {
+//     var d = {};
+//     d['1.2.3'] = new data_obj(1,2,3,4,43,84);
+//     for(var x in ar) {
+//         d['1.2.3'] = new data_obj(1,2,3,4,43,84);
+//     }
+// }
 
 document.getElementById('map_format_select').addEventListener('change', function (event) {
     weather_module.update_format(event.target.value);
@@ -142,49 +116,71 @@ function addData(line) {
 }
 
 function setDataPanel(data) {
-    var dataCount = 0;
-    var cssclass;
-    for (var i = 0, f; f = data[i]; i++) {
-        // output.push('<div class="data">',f,'</div>');
-        cssclass = "data";
-        if(dataCount % 2 == 0) cssclass += " stripe"
-        var temp = '<div class="' + cssclass + '" >' + f + '</div>';
-        addData(temp);
-        dataCount++;
+    if(data == null) {
+        var dataCount = 0;
+        var cssclass;
+        for (var i = 0, f; f = data[i]; i++) {
+            // output.push('<div class="data">',f,'</div>');
+            cssclass = "data";
+            if (dataCount % 2 == 0) cssclass += " stripe"
+            var temp = '<div class="' + cssclass + '" >' + f + '</div>';
+            addData(temp);
+            dataCount++;
+        }
+    } else {
+        addData();
     }
     // document.getElementById('data-panel').innerHTML = output.join('');
 }
 
 function getData(minLat, minLong, maxLat, maxLong) {
-    console.log('getData reached');
-    console.log(minLat + ', ' + minLong + ', ' +  maxLat + ', ' +  maxLong);
-    minLat = Math.round(minLat * 10);
-    minLong = Math.round(minLong * 10);
-    maxLat = Math.round(maxLat * 10);
-    maxLong = Math.round(maxLong * 10);
-    console.log(minLat + ', ' + minLong + ', ' +  maxLat + ', ' +  maxLong);
+    // console.log('getData reached');
+    // console.log(minLat + ', ' + minLong + ', ' +  maxLat + ', ' +  maxLong);
+    // minLat = Math.round(minLat * 10);
+    // minLong = Math.round(minLong * 10);
+    // maxLat = Math.round(maxLat * 10);
+    // maxLong = Math.round(maxLong * 10);
+    console.log('Coordinates: ' + minLat + ', ' + minLong + ', ' +  maxLat + ', ' +  maxLong);
     // var url = 'https://cs420.andrewpe.com/data';
-    var url = 'http://45.55.77.74:8080/get/location/minLat/-20/maxLat/0/minLong/160/maxLong/180';
+    // var url = 'http://45.55.77.74:8080/get/location/minLat/-20/maxLat/0/minLong/160/maxLong/180';
+    // var url = "https://cs420.andrewpe.com/api/get/location/minLat/-20/maxLat/0/minLong/160/maxLong/180";
     // var url = 'http://45.55.77.74:8080/get/location/minLat/${minLat}/maxLat/${maxLat}/minLong/${minLong}/maxLong/${maxLong}';
-    var method = 'GET';
-    var response = 'default text';
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url, false);
-    xhr.timeout = 2000; // time in milliseconds
-    xhr.ontimeout = function (e) {
-        console.log('Timeout');
-        consloe.log(e);
-    };
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            response = JSON.parse(xhr.responseText);
-        } else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) {
-            error("There was an error getting the data!")
-        }
-    }
-    xhr.send();
+    // var method = 'GET';
+    // var response = 'default text';
+    //
+    // var xhr = new XMLHttpRequest();
+    // xhr.open(method, url, false);
+    // xhr.timeout = 10000; // time in milliseconds
+    // xhr.ontimeout = function (e) {
+    //     console.log('Timeout');
+    //     consloe.log(e);
+    // };
+    // xhr.onreadystatechange = function() {
+    //     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    //         response = JSON.parse(xhr.responseText);
+    //     } else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) {
+    //         error("There was an error getting the data!")
+    //     }
+    // }
+    // xhr.send();
+
     // weather_module.load_data(null, response);
-    console.log(response);
+    // console.log(response);
+
+    $.ajax({
+        // url: "http://45.55.77.74:8080/get/location/minLat/-20/maxLat/0/minLong/160/maxLong/180",
+        url: "https://cs420.andrewpe.com/api/get/location/minLat/-20/maxLat/0/minLong/160/maxLong/180",
+        // beforeSend: function( xhr ) {
+        //     xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+        // }
+    })
+        .done(function( data ) {
+
+            if ( console && console.log ) {
+                console.log( "Sample of data:", data.slice( 0, 100 ) );
+                weather_module.load_data(null, response);
+            }
+        });
 }
 
 function error(message) {
